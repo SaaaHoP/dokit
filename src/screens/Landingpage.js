a94874688341a4b8362c629ee39ff7094d98f7c6
SaaaHoP <qkrtjdgh97@gmail.com>
2021-05-15 16:10:45 +0900

    feat/LandingPage
    GlobalStyle, 폴더구조, 아이콘, 이미지 추가

--- src/screens/Landingpage.js
+++ src/screens/Landingpage.js
@@ -1,7 +1,42 @@
 import React from 'react';
+import {
+  LandingPageButtons,
+  LandingPageDokitIcon,
+  LandingPageGetStartedButton,
+  LandingPageImg,
+  LandingPageImgBox,
+  LandingPageLeftBackground,
+  LandingPageLoginButton,
+  LandingPageMainText,
+  LandingPageRightComponents,
+  LandingPageSignUpButton,
+  LandingPageSubText,
+  LandingPageUpperBar,
+} from '../components/LandingPage/styledComponent';
 
-const Landingpage = () => {
-  return <div>Hello~</div>;
+const LandingPage = () => {
+  return (
+    <>
+      <LandingPageLeftBackground />
+      <LandingPageImgBox>
+        <LandingPageImg />
+      </LandingPageImgBox>
+      <LandingPageUpperBar>
+        <LandingPageDokitIcon />
+        <LandingPageButtons>
+          <LandingPageLoginButton>Login</LandingPageLoginButton>
+          <LandingPageSignUpButton>Sign Up</LandingPageSignUpButton>
+        </LandingPageButtons>
+      </LandingPageUpperBar>
+      <LandingPageRightComponents>
+        <LandingPageMainText>Use DokitList</LandingPageMainText>
+        <LandingPageSubText>
+          Our Todolist is easy to use, comfortable and free.
+        </LandingPageSubText>
+        <LandingPageGetStartedButton>Get Started</LandingPageGetStartedButton>
+      </LandingPageRightComponents>
+    </>
+  );
 };
 
-export default Landingpage;
+export default LandingPage;
