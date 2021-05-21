0fc93932d9bc1b65d1f04d9084f0b9634784670d
JIWON_MIN <mjw3534@gmail.com>
2021-05-21 16:07:00 +0900

    feat/FriendPage
    초기 기본 템플릿만

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import LandingPage from './screens/LandingPage';
 import LoginPage from './screens/LoginPage';
 import RegisterPage from './screens/RegisterPage';
+import FriendPage from './screens/FriendPage';
 import GlobalStyle from './styles/GlobalStyle';
 import MainPage from './screens/MainPage';
 import { Route } from 'react-router-dom';
@@ -9,9 +10,9 @@ function App() {
   return (
     <>
       <GlobalStyle />
+      <Route path='/register' component={RegisterPage} />
       <Route path='/' component={LandingPage} exact />
       <Route path='/login' component={LoginPage} />
-      <Route path='/register' component={RegisterPage} />
       <Route path='/main' component={MainPage} />
     </>
   );
