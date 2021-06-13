7235f68f0649cfc2bc53a3503bb66c2b3b9f94b9
JIWON_MIN <mjw3534@gmail.com>
2021-06-14 01:08:26 +0900

    팀페이지 임시 수정

--- src/components/TeamPage/TeamComponent.js
+++ src/components/TeamPage/TeamComponent.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link, Route } from 'react-router-dom';
 import axios from 'axios';
 import { address } from '../../variables';
 import {
@@ -32,15 +33,17 @@ const TeamComponent = () => {
 
     const FavoriteTeamListElementCreator = ({user}) => {
         return(
+            <Link to='/teamCalendar'>
             <TeamPageListElement>
                 <TeamPageFavoriteStar/>
                 <TeamPageUserProfileCircle src={user.profileUrl}/>
                 <TeamPageUserName>{user.teamName}</TeamPageUserName>
                 <TeamPageUserComment>{user.projectName}</TeamPageUserComment>
             </TeamPageListElement>
+            </Link>
         );
     }
-    
+
     const FavoriteTeamListCreator = ({users}) => {
         return(
             <>
@@ -61,7 +64,7 @@ const TeamComponent = () => {
             </TeamPageListElement>
         );
     }
-    
+
     const TeamListCreator = ({users}) => {
         return(
             <>
@@ -71,7 +74,7 @@ const TeamComponent = () => {
             </>
         );
     }
-    
+
     const TeamRequestElementCreator = ({user}) => {
         return(
             <TeamPageListElement>
@@ -80,7 +83,7 @@ const TeamComponent = () => {
                 <TeamPageAcceptButton onClick={(e) => acceptHandler(user.id, e)}>
                     Accept
                 </TeamPageAcceptButton>
-                <TeamPageRejectButton onClick={(e) => rejectHandler(user.id, e)}> 
+                <TeamPageRejectButton onClick={(e) => rejectHandler(user.id, e)}>
                     Reject
                 </TeamPageRejectButton>
             </TeamPageListElement>
@@ -93,10 +96,10 @@ const TeamComponent = () => {
            teamId: id
         });
         await axios.get(`${address}/teams/me/request`).then((res) => {
-            setTeamRequestElements(res.data.teams);
+            ////setTeamRequestElements(res.data.teams);
         });
         await axios.get(`${address}/teams/me/normal`).then((res) => {
-            setTeamListElements(res.data.teams);
+            //setTeamListElements(res.data.teams);
         });
     };
 
@@ -108,13 +111,13 @@ const TeamComponent = () => {
             }
         });
         await axios.get(`${address}/teams/me/request`).then((res) => {
-            setTeamRequestElements(res.data.teams);
+            //setTeamRequestElements(res.data.teams);
         });
         await axios.get(`${address}/teams/me/normal`).then((res) => {
-                setTeamListElements(res.data.teams);
+                //setTeamListElements(res.data.teams);
             });
     };
-    
+
     const TeamRequestCreator = ({users}) => {
         return(
             <>
@@ -125,29 +128,78 @@ const TeamComponent = () => {
         );
     }
 
-    var [FavoriteTeamListElements, setFavoriteTeamListElements] = useState([]);
-    var [TeamListElements, setTeamListElements] = useState([]);
-    var [TeamRequestElements, setTeamRequestElements] = useState([]);
+    // var [FavoriteTeamListElements, //setFavoriteTeamListElements] = useState([]);
+    // var [TeamListElements, //setTeamListElements] = useState([]);
+    // var [TeamRequestElements, //setTeamRequestElements] = useState([]);
 
-    useEffect(() => {
-        const axiosGet = async () => {
-            let jwtToken = localStorage.getItem('Authorization');
-            axios.defaults.headers.common['Authorization'] = jwtToken;
+    // useEffect(() => {
+    //     const axiosGet = async () => {
+    //         let jwtToken = localStorage.getItem('Authorization');
+    //         axios.defaults.headers.common['Authorization'] = jwtToken;
 
-            await axios.get(`${address}/teams/me/favorite`).then((res) => {
-                setFavoriteTeamListElements(res.data.teams);
-            });
+    //         await axios.get(`${address}/teams/me/favorite`).then((res) => {
+    //             //setFavoriteTeamListElements(res.data.teams);
+    //         });
 
-            await axios.get(`${address}/teams/me/normal`).then((res) => {
-                setTeamListElements(res.data.teams);
-            });
+    //         await axios.get(`${address}/teams/me/normal`).then((res) => {
+    //             //setTeamListElements(res.data.teams);
+    //         });
 
-            await axios.get(`${address}/teams/me/request`).then((res) => {
-                setTeamRequestElements(res.data.teams);
-            });
-        };
-        axiosGet();
-    },[]);
+    //         await axios.get(`${address}/teams/me/request`).then((res) => {
+    //             //setTeamRequestElements(res.data.teams);
+    //         });
+    //     };
+    //     axiosGet();
+    // },[]);
+    const FavoriteTeamListElements = [
+        {
+            id: 1,
+            teamName: '두야~호~!',
+            projectName: 'DOKIT',
+            favorite: 'true'
+        },
+        {
+            id: 2,
+            teamName: '19쿼드라',
+            projectName: '인공지능 8조',
+            favorite: 'true'
+        },
+    ];
+    const TeamListElements = [
+        {
+            id: 1,
+            teamName: '객체 2조',
+            projectName: 'Car Rental Service',
+            favorite: 'false'
+        },
+        {
+            id: 2,
+            teamName: '객체 9조',
+            projectName: '미정',
+            favorite: 'false'
+        },
+        {
+            id: 2,
+            teamName: '네프 8조',
+            projectName: 'Socket Programming',
+            favorite: 'false'
+        },
+    ];
+    const TeamRequestElements = [
+        {
+            id: 1,
+            teamName: '스터디그룹 SP',
+        },
+        {
+            id: 2,
+            teamName: '댄스 동아리 DOPOP',
+        },
+        {
+            id: 2,
+            teamName: '프로그래밍 가반'
+        },
+    ];
+
 
     return (
 
